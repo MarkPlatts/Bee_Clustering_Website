9f7db2cd771632dc63886734f4240605267897de
MarkPlatts <markplatts23@gmail.com>
2018-07-17 16:20:00 -0400

    Changed R src files so that it improves the look of plots.

--- code/R/plot_prop_each_cluster.R
+++ code/R/plot_prop_each_cluster.R
@@ -3,13 +3,14 @@ library(here)
 
 plot_prop_each_cluster <- function(segment_length, n_clusters){
 
-  path <- paste0("Data/length", segment_length, "/")
+  path <- paste0(here::here("Data/length"), segment_length, "/")
   df <- read.csv(paste0(path, "xy_voted_", n_clusters, "_clusters.csv"))
   df$Cluster <- as.factor(df$cluster_won + 1)
   ggplot(data = df, aes(x = Experiment, fill = Cluster)) +
     geom_bar(position = "fill") +
     ggtitle(paste0("Segment Length: ", segment_length)) +
-    coord_flip() + labs(x = "Percentage of path")
+    coord_flip() + labs(y = "Percentage of path", x = "Treatment") +
+    theme_bw(base_size = 10)
 
 }
 
